f41e84f312716af64b8cebaf0f009e032f9dd629
Abhi <Bh00fie@gmail.com>
2023-03-06 21:29:46 +0000

    Defined Variables containing classes and ids from HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+// Defined Variables containing classes and ids from HTML
+const searchForm = $('#search-form');
+const searchInput = $('#search-input');
+const searchBtn = $('#search-button');
+const historyList = $('#history');
+const forecastHead = $('#forecast-header');
+const forecastList = $('#forecast').empty();
+const todayHead = $('#current-header');
+const todayWeather = $('#today').empty();
+const cityArray = [];
