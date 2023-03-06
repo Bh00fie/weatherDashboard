9d1bde1efd05141b3c8ef8f6f1923f60d9b71c06
Abhi <Bh00fie@gmail.com>
2023-03-06 21:30:15 +0000

    Defined API key

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ const forecastList = $('#forecast').empty();
 const todayHead = $('#current-header');
 const todayWeather = $('#today').empty();
 const cityArray = [];
+
+// Defined API key
+const apiKey = '5db739fbbf70439b82102c73fb943ced';
